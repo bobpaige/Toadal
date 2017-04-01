bcf8c176190336a9d7f4bda7bfe801a8044555bd
bob.paige <bob.paige@coxautoinc.com>
2017-04-01 14:03:00 -0400

    add player relationships

--- src/main/java/com/example/ToadalApplication.java
+++ src/main/java/com/example/ToadalApplication.java
@@ -1,12 +1,34 @@
 package com.example;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.model.Game;
+import com.example.model.Player;
+import com.example.persistence.GameRepository;
+import com.example.persistence.PlayerRepository;
 
 @SpringBootApplication
 public class ToadalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ToadalApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner loadGames(GameRepository grepo, PlayerRepository prepo) {
+		return new CommandLineRunner() {
+
+			@Override
+			public void run(String... args) throws Exception {
+				Player player1 = Player.builder().name("joe").build();
+				prepo.save(player1);
+				Game game1 = Game.builder().name("mygame").build();
+				game1.getPlayers().add(player1);
+				grepo.save(game1);
+			}
+		};
 	}
 }
--- src/main/java/com/example/model/Game.java
+++ src/main/java/com/example/model/Game.java
@@ -1,5 +1,6 @@
 package com.example.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import javax.persistence.Entity;
@@ -8,16 +9,27 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@NoArgsConstructor(force = true)
+@AllArgsConstructor
 public class Game {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String name;
-	
+
+	@Builder
+	public Game(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
 	@OneToMany
-	private Set<Player> players;
+	private Set<Player> players = new HashSet<>();
 }
